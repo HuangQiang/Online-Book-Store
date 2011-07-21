269940add41ee18df983bbf99d80889df5b06e0d
YYFei <375290336@qq.com>
2011-07-21 17:00:00 +0800

    Yu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '2a3612da5c0d27b683c203aaa879081610d7ce68bccb047ded156031c376ae658f63256568d6e1760c2aa4065ff219ffcf110f58249e2c9c7b14b18c73fefacd'
