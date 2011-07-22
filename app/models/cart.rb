b644f68ce4889c25c9e42541a3558632596c0fc8
YYFei <375290336@qq.com>
2011-07-22 15:34:07 +0800

    test

--- app/models/cart.rb
+++
@@ -1,7 +0,0 @@
-class Cart < ActiveRecord::Base
-<<<<<<< HEAD
-  has_many :line_items,:dependent => :destroy
-=======
-  has_many :line_items, :dependent => :destroy
->>>>>>> 269940add41ee18df983bbf99d80889df5b06e0d
-end
