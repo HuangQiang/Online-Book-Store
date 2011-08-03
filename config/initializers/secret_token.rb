e1b1345f98878768ac65e90c66bde89a6440ae56
lzhyou <08386042@163.com>
2011-08-03 15:47:15 +0800

    advanced carts

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Depot::Application.config.secret_token = '2a3612da5c0d27b683c203aaa879081610d7ce68bccb047ded156031c376ae658f63256568d6e1760c2aa4065ff219ffcf110f58249e2c9c7b14b18c73fefacd'
+Depot::Application.config.secret_token = 'b156482acec4cdcbee19ccd00afe64440799e87e0dce997c59a17bd5d4101de418988bb438b4d2b7d8fbf3e8bdd494c7e4d03aafe04b93423b3b7490c69c9b66'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
 # encoding: utf-8
 Product.delete_all
 Product.create(:title => 'Web Design for Developers',
