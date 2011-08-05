2744ba75c64ed20670c27672ad1ee02742a62dda
zff <970251106@qq.com>
2011-08-05 11:30:25 +0800

    zff

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,11 @@
 class OrdersController < ApplicationController
+  skip_before_filter :authorize, :only => [:new, :create]
   # GET /orders
   # GET /orders.xml
-  skip_before_filter :authorize,:only => [:new,:create]
   def index
-#    @orders = Order.all
-    @orders = Order.paginate :page=>params[:page], :order=>'created_at desc',:per_page =>10
-
+    @orders = Order.paginate :page=>params[:page], :order=>'created_at desc',
+	  :per_page =>10
+	  
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @orders }
@@ -27,10 +27,10 @@ class OrdersController < ApplicationController
   # GET /orders/new.xml
   def new
     if current_cart.line_items.empty?
-        redirect_to store_url,:notice => "Your cart is empty"
-        return
-    end
-    
+	  redirect_to store_url, :notice => "Your cart is empty"
+	  return
+	end
+	
     @order = Order.new
 
     respond_to do |format|
@@ -48,15 +48,13 @@ class OrdersController < ApplicationController
   # POST /orders.xml
   def create
     @order = Order.new(params[:order])
-    @order.add_line_items_from_cart(current_cart)
+	@order.add_line_items_from_cart(current_cart)
 
     respond_to do |format|
       if @order.save
-        Cart.destroy(session[:cart_id])
-        session[:cart_id] = nil
-        
-        Notifier.order_received(@order).deliver
-        
+	    Cart.destroy(session[:cart_id])
+		session[:cart_id] = nil
+		Notifier.order_received(@order).deliver
         format.html { redirect_to(store_url, :notice => I18n.t('.thanks')) }
         format.xml  { render :xml => @order, :status => :created, :location => @order }
       else
