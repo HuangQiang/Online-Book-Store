842739668c9e7887426af96a1de07ada1ca7da88
lipei <1036129755@qq.com>
2011-08-11 10:49:51 +0800

    null

--- config/environment.rb
+++
@@ -1,7 +0,0 @@
-# Load the rails application
-require File.expand_path('../application', __FILE__)
-
-# Initialize the rails application
-Depot::Application.initialize!
-
-#config.action_mailer.delivery_method = :smtp | :sendmail | :test
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Depot::Application.config.secret_token = 'ad2599d4ae2df7ec3c2f4dfa0088f072ce1c63713b1551965cef89e76c90f1cbe602940f7268ac7cbcfaa34335c794491d6fd9ad05588055a69e3ec44e85e299'
